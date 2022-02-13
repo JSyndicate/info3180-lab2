caf8c9eb27d4d2203fcad2f7ea8b10620c5972d4
JSyndicate <jevwilliamson@gmail.com>
2022-02-13 13:47:50 -0500

    your commit message

--- app/views.py
+++ app/views.py
@@ -7,11 +7,22 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
 # Routing for your application.
 ###
+
+@app.route('/profile/')
+def profile():
+    """Render website's profile page."""
+    return render_template('profile.html', todaysdate=format_date_joined(datetime.datetime.now()))
+
+def format_date_joined(date):
+    now=datetime.datetime.now()
+    date_joined=datetime.date(2022, 2, 13)
+    return "Joined" + date_joined.strftime("%B, %Y")
 
 @app.route('/')
 def home():
